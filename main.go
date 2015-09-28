e578649ac8cab6dead380fc9b9ce214924ae6b98
soyking <soyking@outlook.com>
2015-09-28 22:21:18 +0800

    init

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	_ "MomentAPI/docs"
+	_ "MomentAPI/routers"
+
+	"github.com/astaxie/beego"
+)
+
+func main() {
+	if beego.RunMode == "dev" {
+		beego.DirectoryIndex = true
+		beego.StaticDir["/swagger"] = "swagger"
+	}
+	beego.Run()
+}
